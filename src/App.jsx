07950136d66cffb3dd7cdd8ab308debc748bbd49
Giovanni Donati <grdonati99@gmail.com>
2024-10-16 06:17:44 -0300

    adicionando onCLick

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,25 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId){
+    const newTask = tasks.map(task => {
+      if(task.id === taskId){
+        return {...task, isCompleted: !task.isCompleted}
+      }
+      return task
+    });
+    setTasks(newTask)
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
-      <h1 className="text-3xl text-slate-100 font-bold text-center">
+      <h1 className="text-3xl text-slate-100 font-bold text-center p-4">
         Control Tasks
       </h1>
 
       <AddTasks/>
-      <Tasks Tasks={tasks}/>
+      <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
       </div>
     </div>
   );
