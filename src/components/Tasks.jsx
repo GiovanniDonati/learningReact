9dfddfd3beb41bd658a7b5820537a59c1d3cdabf
Giovanni_DeskEmp <grdonati99@gmail.com>
2024-10-23 08:42:35 -0300

    details TaskPage and button returm home

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,42 +1,46 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}){
-    const navigate = useNavigate();
+  function onSeeDetailsTask(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/taskpage?${query.toString()}`);
+  }
 
-    function onSeeDetailsTask(task){
-        const query = new URLSearchParams()
-        query.set('title', task.title)
-        query.set('description', task.description)
-        navigate(`/taskpage?title=${query.toString()}`);
-    }
-
-    return ( 
+  return (
     <ul className="bg-blue-100 space-y-4 p-6 bg-white-100 rounded-md shadow">
-        {tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-            <button 
-                onClick={() => onTaskClick(task.id)}
-                className={`bg-white w-full text-left text-base font-medium text-black-500 p-2 rounded-md hover:bg-gray-100 
-                            ${task.isCompleted && 'line-through text-gray-500'}`}>
-                    {task.title}
-            </button>
-            <button 
-                onClick={() => onSeeDetailsTask(task)}
-                className="bg-blue-800 text-white p-2 rounded-md text-base font-medium w-10 hover:bg-blue-500">
-                <ChevronRightIcon/>
-            </button>
-            
-            <button 
-                onClick={() => onDeleteTaskClick(task.id)}
-                className="bg-blue-800 text-white p-2 rounded-md text-base font-medium w-10 hover:bg-blue-500">
-                <TrashIcon/>
-            </button>
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-white w-full text-left text-base font-medium text-black-500 p-2 rounded-md hover:bg-gray-100 
+                            ${
+                              task.isCompleted && "line-through text-gray-500"
+                            }`}
+          >
+            {task.title}
+          </button>
+          <button
+            onClick={() => onSeeDetailsTask(task)}
+            className="bg-blue-800 text-white p-2 rounded-md text-base font-medium w-10 hover:bg-blue-500"
+          >
+            <ChevronRightIcon />
+          </button>
+
+          <button
+            onClick={() => onDeleteTaskClick(task.id)}
+            className="bg-blue-800 text-white p-2 rounded-md text-base font-medium w-10 hover:bg-blue-500"
+          >
+            <TrashIcon />
+          </button>
         </li>
-    ))}    
+      ))}
     </ul>
-);
-};
+  );
+}
 
 export default Tasks;
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,23 +1,41 @@
 import { ArrowLeftCircleIcon } from "lucide-react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
-function TaskPage(){
-    const [searchParams] = useSearchParams();
-    const title = searchParams.get("title");
-    const description = searchParams.get("description");
-    
-    return (
-        <div className="h-screen w-screen bg-white p-6">
-            <div className="w-[500] space-y-4">
-                < ArrowLeftCircleIcon className="absolute bg-blue-800 text-white p-2 rounded-md w-10 h-10 hover:bg-blue-500"/>
-                <h1 className="text-3xl font-bold text-center text-black">Detalhes da Tarefa</h1>
-                <div className="bg-blue-100 p-4 rounded-md space-y-4">
-                    <h2 className="p-2 bg-white text-black text-xl font-bold rounded-md">{title}</h2>
-                    <p className="p-2 bg-white text-black text-base font-bold">{description}</p>
-                </div>
-            </div>
+function TaskPage() {
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  console.log(title);
+
+  const navigate = useNavigate();
+  function returnHome() {
+    navigate("/");
+  }
+
+  return (
+    <div className="h-screen w-screen bg-white p-6 flex justify-center ">
+      <div className="w-[500] space-y-4">
+        <h1 className="text-3xl font-bold text-center text-black">
+          Detalhes da Tarefa
+        </h1>
+        <div className="w-[500px] bg-blue-100 p-4 rounded-md shadow ">
+          <h2 className="p-2 bg-slate-50 text-black text-center text-xl font-bold rounded-t-md">
+            {title}
+          </h2>
+          <hr className="w-full" />
+          <p className="p-2 bg-slate-50 text-black text-sm text-center rounded-b-md">
+            {description}
+          </p>
+          <button
+            onClick={returnHome}
+            className="bg-blue-800 text-white flex gap-2 p-2 rounded-md w-24 h-10 hover:bg-blue-500 mt-2"
+          >
+            <ArrowLeftCircleIcon />
+            Voltar
+          </button>
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
-export default TaskPage
+export default TaskPage;
